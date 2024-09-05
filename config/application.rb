69d92d1ddf593cf20c7a3ff9b4171bb42c784ffe
mamun jan <mamunnabijan@gmail.com>
2024-09-06 00:43:29 +0300

    clcikable gyms results redirecting to websit

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
-require 'dotenv/load'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,15 +9,7 @@ Bundler.require(*Rails.groups)
 module Fitpal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
-    # Ensure the services directory is autoloaded
-    config.autoload_paths += %W(#{config.root}/app/services)
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.load_defaults 7.1
 
     # Configuration for the application, engines, and railties goes here.
     #
